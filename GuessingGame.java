6b16fa3200346c0a8256580126f414f8ccaf80b4
jensbankel <jenban1204@skola.goteborg.se>
2020-10-19 22:46:49 +0200

    changed arguments to pass to Guesser constructor

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,6 @@
 public class GuessingGame {
     public static void main(String args[]) {
-        Guesser game = new Guesser(0, 1000);
+        Guesser game = new Guesser(0, 10);
         game.start();
     }
 }
