285dc432923c48021e17393e66a16af50c1c0a73
jensbankel <jenban1204@skola.goteborg.se>
2020-10-20 12:43:22 +0200

    made a script

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,22 @@
  #!/bin/bash
 
- echo "Jens Bankel"
+ NAME="Jens_Bankel"
+ SUFFIX="_labb"
+ CATALOG="$NAME$SUFFIX"
+
+ echo "$NAME"
+
+ mkdir "$CATALOG"
+ 
+ for file in *.java
+ do
+ cp "$file" "$CATALOG"
+ done
+
+ cd "$CATALOG"
+ pwd
+
+
+
+
+
